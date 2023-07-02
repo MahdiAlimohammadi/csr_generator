a227d1f57894ace178e4992505bbfc6af7e92531
Mahdi Alimohammadi <mahdi.alimohammadi1992@gmail.com>
2023-07-02 20:43:42 +0330

    [Edit] output

--- csr_generation.py
+++ csr_generation.py
@@ -17,11 +17,8 @@ def generate_csr_interactive():
         domain_directory = common_name.replace(".", "_")
         os.makedirs(domain_directory, exist_ok=True)
 
-        # Add common_name at the beginning of file names
-        common_name_prefix = common_name.replace(".", "_")
-
         # Create the conf file
-        conf_file = os.path.join(domain_directory, f"{common_name_prefix}_mycsr.conf")
+        conf_file = os.path.join(domain_directory, f"{common_name}_mycsr.conf")
         with open(conf_file, "w") as f:
             f.write(f"""[req]
 default_bits = 2048
@@ -30,13 +27,13 @@ prompt             = no
 req_extensions = req_ext
 
 [dn]
-C={country}
-ST={state}
-L={city}
-O={organization}
-OU={organizational_unit}
-emailAddress={email}
-CN={common_name}
+countryName                = {country}
+stateOrProvinceName        = {state}
+localityName               = {city}
+organizationName           = {organization}
+organizationalUnitName     = {organizational_unit}
+commonName                 = {common_name}
+emailAddress               = {email}
 
 [req_ext]
 subjectAltName = @alt_names
@@ -46,19 +43,51 @@ DNS.0 = *.{common_name}
 DNS.1 = *.dev.{common_name}
 """)
 
-        # Generate the private key
-        private_key_file = os.path.join(domain_directory, f"{common_name_prefix}_private.key")
-        openssl_key_cmd = f"openssl genpkey -algorithm RSA -out {private_key_file}"
-        subprocess.run(openssl_key_cmd, shell=True, check=True)
+        generate_csr_from_config(conf_file)
 
-        # Generate the CSR
-        csr_file = os.path.join(domain_directory, f"{common_name_prefix}_csr.pem")
-        openssl_csr_cmd = f"openssl req -new -config {conf_file} -key {private_key_file} -out {csr_file}"
-        subprocess.run(openssl_csr_cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while executing OpenSSL commands:")
+        print(e.stderr.decode().strip())
+    except Exception as e:
+        print("An error occurred while generating the CSR:")
+        print(str(e))
 
-        print("\nCSR generated successfully!")
-        print(f"CSR file: {os.path.abspath(csr_file)}")
-        print(f"Private key file: {os.path.abspath(private_key_file)}")
+
+def generate_csr_from_config(conf_file):
+    try:
+        domain_directory = os.path.dirname(conf_file)
+        common_name = None
+
+        # Read the commonName from the configuration file
+        with open(conf_file, "r") as f:
+            for line in f:
+                if line.strip().startswith("commonName"):
+                    common_name = line.strip().split("=")[1].strip().strip('"')
+                    break
+
+        if common_name:
+            # Create the directory with the domain name
+            domain_directory = common_name.replace(".", "_")
+            os.makedirs(domain_directory, exist_ok=True)
+
+            # Add common_name at the beginning of file names
+            common_name_prefix = common_name.replace(".", "_")
+
+            # Generate the private key
+            private_key_file = os.path.join(domain_directory, f"{common_name_prefix}_private.key")
+            openssl_key_cmd = f"openssl genpkey -algorithm RSA -out {private_key_file}"
+            subprocess.run(openssl_key_cmd, shell=True, check=True)
+
+            # Generate the CSR
+            csr_file = os.path.join(domain_directory, f"{common_name_prefix}_csr.pem")
+            openssl_csr_cmd = f"openssl req -new -config {conf_file} -key {private_key_file} -out {csr_file}"
+            subprocess.run(openssl_csr_cmd, shell=True, check=True)
+
+            print("\nCSR generated successfully!")
+            print(f"CSR file: {os.path.abspath(csr_file)}")
+            print(f"Private key file: {os.path.abspath(private_key_file)}")
+        else:
+            print("Error: Common Name (commonName) not found in the configuration file.")
 
     except subprocess.CalledProcessError as e:
         print("An error occurred while executing OpenSSL commands:")
@@ -80,18 +109,7 @@ def main():
     elif choice == "2":
         try:
             conf_file = input("Enter the path to the configuration file: ")
-            domain_directory = os.path.dirname(conf_file)
-
-            openssl_key_cmd = f"openssl genpkey -algorithm RSA -out {os.path.join(domain_directory, 'private.key')}"
-            openssl_csr_cmd = f"openssl req -new -config {conf_file} -key {os.path.join(domain_directory, 'private.key')} -out {os.path.join(domain_directory, 'csr.pem')}"
-
-            subprocess.run(openssl_key_cmd, shell=True, check=True)
-            subprocess.run(openssl_csr_cmd, shell=True, check=True)
-
-            print("\nCSR generated successfully!")
-        except subprocess.CalledProcessError as e:
-            print("An error occurred while executing OpenSSL commands:")
-            print(e.stderr.decode().strip())
+            generate_csr_from_config(conf_file)
         except Exception as e:
             print("An error occurred while generating the CSR:")
             print(str(e))
